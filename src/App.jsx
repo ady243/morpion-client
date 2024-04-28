f4fb56c254852565f0497f960f7d1b5652d28e06
ady2024 <amasivi@uzer.eu>
2024-04-28 16:39:06 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h1  className='text-red-500 text-center'>Welcom to the morpion game</h1>
+      </div>
+       
+    </>
+  )
+}
+
+export default App
