7c87a8e951c13cfc564f319ce2f3a7b020d342c8
ady2024 <amasivi@uzer.eu>
2024-05-13 14:30:00 +0200

    refacto

--- src/component/loader/ConfirmLoad.jsx
+++ src/component/loader/ConfirmLoad.jsx
@@ -1,31 +1,15 @@
-import { useState, useEffect } from 'react'
-import { apiAuhentication } from '../../utils/services';
 import "./css/confirm.css"
 
-function ConfirmLoad() {
-  const [validateToken, setValidateToken] = useState('');
-  const [isEmailSent, setIsEmailSent] = useState(false);
-  useEffect(() => {
-    const fetchValidateToken = async () => {
-      const response = await fetch(`${apiAuhentication}/users/register`);
-      const data = await response.json();
-      setTimeout(() => {
-        setValidateToken(data.validateToken);
-        if (!data.validateToken) {
-          setIsEmailSent(true);
-        }
-      }, 120000);
-    };
-  
-    fetchValidateToken();
-  }, []);
 
+function ConfirmLoad() {
   return (
-    <>
-      {validateToken && <div className="loader"></div>}
-      {isEmailSent && <div>Un email de confirmation vous a été envoyé</div>}
-    </>
-  );
+  <>
+    <div>
+      <div className="loader"></div>
+    </div>
+  </>
+
+  )
 }
 
-export default ConfirmLoad;
+export default ConfirmLoad
