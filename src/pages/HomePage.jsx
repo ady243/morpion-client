3159969900ad0bfb3222ff37c7dfc6c287adbb27
ady243 <masiviady7@gmail.com>
2024-05-23 13:02:27 +0200

    message pour le commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import {Textarea} from "@nextui-org/react";
+// import {Textarea} from "@nextui-org/react";
 import NavBar from "../component/NavBar.jsx";
 import Game from "../component/Geme.jsx";
 import { AuthContext } from "../context/AuthContext.jsx";
@@ -24,14 +24,14 @@ const { currentUser } = useContext(AuthContext);
 
                 <Game/>
             </div>
-            <div className="flex items-center justify-center h-screen -mt-96">
+            {/* <div className="flex items-center justify-center h-screen -mt-96">
                 <Textarea
                     labelPlacement="outside"
                     placeholder="Tapez votre message ici ..."
                     className="col-span-12 md:col-span-6 mb-6 md:mb-0 rounded-tl-xl w-5/12 h-1/2 mt-32"
                     style={textAreaStyle}
                 />
-            </div>
+            </div> */}
         </>
     );
 }
